d669d171b80bdef461f339f5b8d57a7a32a8e9e0
Marino Matteo <s4544717@studenti.unige.it>
2025-11-15 12:19:38 +0100

    functions that handles data form users moved to statsHandler.js

---
+++ HeartBeatMaster/Local/Public/statsHandler.js
@@ -0,0 +1,24 @@
+export function calcIntensity(hr, hrMax, hrRest) {
+  const hrr = hrMax - hrRest;
+  const intensity = ((hr - hrRest) / hrr) * 100;
+  return intensity.toFixed(2);
+}
+
+export function calcHeartRateMax(bornDate) {
+  const birthYear = new Date(bornDate).getFullYear();
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  return 208 -0.7*age;
+}
+
+export function getHeartRateMin(male) {
+  if(male==="1"){
+    return 0.64;
+  } else if(male==="0"){
+    return 0.76;
+  }
+  else
+  {
+    throw new Error("Invalid value for sex parameter: "+ male);
+  }
+}
