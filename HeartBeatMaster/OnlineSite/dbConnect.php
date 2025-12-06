8d38c1fb32447e5d4ab4c8d179b6bd9ee58a68da
Marino Matteo <s4544717@studenti.unige.it>
2025-12-07 00:23:36 +0100

    add tables to handle workout data

--- HeartBeatMaster/OnlineSite/dbConnect.php
+++ HeartBeatMaster/OnlineSite/dbConnect.php
@@ -12,6 +12,7 @@ $datiMonitoraggioUtenti__TABLE = 'datimonitoraggioutenti';
     $datiMonitoraggioUtenti_altezza__COLUMN = 'altezza';
     $datiMonitoraggioUtenti_peso__COLUMN = 'peso';
     $datiMonitoraggioUtenti_ID_utente__COLUMN = 'ID_utente';
+
 $utenti__TABLE = 'utenti';
     $utenti_ID__COLUMN = 'ID';
     $utenti_nome__COLUMN = 'nome';
@@ -21,6 +22,33 @@ $utenti__TABLE = 'utenti';
     $utenti_data_nascita__COLUMN = 'data_nascita';
     $utenti_sesso__COLUMN = 'sesso';
 
+$workout__TABLE = 'workout';
+    $workout_ID__COLUMN = 'ID';
+    $workout_data_inizio__COLUMN = 'data_inizio';
+    $workout_data_fine__COLUMN = 'data_fine';
+    $workout_ID_tipologiaWorkout__COLUMN = 'ID_tipologiaworkout';
+
+$tipologieWorkout__TABLE = 'tipologieworkout';
+    $tipologieWorkout_ID__COLUMN = 'ID';
+    $tipologieWorkout_nome__COLUMN = 'nome';
+    $tipologieWorkout_descrizione__COLUMN = 'descrizione';
+    $datiWorkout_durata_intervallo_analisi__COLUMN = 'durata_intervallo_analisi';
+
+$partecipanteWorkout__TABLE = 'partecipanteworkout';
+    $partecipanteWorkout_ID__COLUMN = 'ID';
+    $partecipanteWorkout_ID_workout__COLUMN = 'ID_workout';
+    $partecipanteWorkout_ID_utente__COLUMN = 'ID_utente';
+    $partecipanteWorkout__ID_fascia__COLUMN = 'ID_fascia';  //for history
+    $partecipanteWorkout_altezza__COLUMN = 'altezza';       //for history
+    $partecipanteWorkout_peso__COLUMN = 'peso';             //for history
+
+$datiWorkout__TABLE = 'datiworkout';
+    $datiWorkout_ID__COLUMN = 'ID';
+    $datiWorkout_ID_partecipanteWorkout__COLUMN = 'ID_partecipanteworkout';
+    $datiWorkout_num_intervallo__COLUMN = 'num_intervallo';
+    $datiWorkout_frequenza_cardiaca__COLUMN = 'frequenza_cardiaca';
+    $datiWorkout_calorie_bruciate__COLUMN = 'calorie_bruciate';
+    $datiWorkout_intesita__COLUMN = 'intensita';
 function connectToDatabase() {
     
     $config = require __DIR__ . '/config.php';
